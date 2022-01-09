195e83c497df9010f55bfd5802cc46843c3118da
Michal Stehlík <michal.stehlik@pslib.cz>
2022-01-09 21:37:35 +0100

    Přidejte soubory projektu.

---
+++ LINQDemo2022/Model/Mark.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo2022.Model
+{
+    internal class Mark
+    {
+        public Mark(string firstName, string lastName, string className, string subject, int value, DateTime created)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ClassName = className;
+            Subject = subject;
+            Value = value;
+            Created = created;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public string Subject { get; set; }
+        public int Value { get; set; }
+        public DateTime Created { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Student {0} {1} ({3}), {4}, {5} ({6})", FirstName, LastName, ClassName, Subject, Value, Created);
+        }
+    }
+}
---
+++ LINQDemo2022/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LINQDemo2022.Model;
+
+// Alice Andělová P1
+// Bořivoj Bohatý P1
+// Ctirad Cudný P2
+// Daniela Drozdová P2
+
+// ALG, PRG, MAT, CJL
+
+// https://docs.microsoft.com/cs-cz/dotnet/csharp/programming-guide/concepts/linq/
+// https://www.tutorialsteacher.com/linq
+// https://www.geeksforgeeks.org/linq-method-syntax/?ref=lbp
+
+List<Mark> Marks = new List<Mark> { 
+    new Mark("Alice","Andělová", "P1", "ALG", 1, new DateTime(2020,1,1)),
+    new Mark("Bořivoj","Bohatý", "P1", "ALG", 3, new DateTime(2020,1,1)),
+    new Mark("Ctirad","Cudný", "P2", "PRG", 1, new DateTime(2020,2,2)),
+    new Mark("Daniela","Drozdová", "P2", "PRG", 5, new DateTime(2020,2,2)),
+    new Mark("Alice","Andělová", "P1", "MAT", 2, new DateTime(2020,3,1)),
+    new Mark("Bořivoj","Bohatý", "P1", "MAT", 3, new DateTime(2020,4,7)),
+    new Mark("Ctirad","Cudný", "P2", "MAT", 3, new DateTime(2020,2,2)),
+    new Mark("Daniela","Drozdová", "P2", "MAT", 5, new DateTime(2020,1,2)),
+    new Mark("Alice","Andělová", "P1", "MAT", 2, new DateTime(2020,1,1)),
+    new Mark("Bořivoj","Bohatý", "P1", "MAT", 4, new DateTime(2020,1,1)),
+    new Mark("Ctirad","Cudný", "P2", "PRG", 2, new DateTime(2020,2,2)),
+    new Mark("Daniela","Drozdová", "P2", "PRG", 5, new DateTime(2020,2,2)),
+    new Mark("Alice","Andělová", "P1", "ALG", 4, new DateTime(2020,1,1)),
+    new Mark("Bořivoj","Bohatý", "P1", "ALG", 4, new DateTime(2020,1,1)),
+    new Mark("Ctirad","Cudný", "P2", "CJL", 2, new DateTime(2020,2,2)),
+    new Mark("Daniela","Drozdová", "P2", "CJL", 5, new DateTime(2020,2,2)),
+    new Mark("Alice","Andělová", "P1", "CJL", 1, new DateTime(2020,3,1)),
+    new Mark("Bořivoj","Bohatý", "P1", "CJL", 3, new DateTime(2020,4,4)),
+    new Mark("Ctirad","Cudný", "P2", "MAT", 1, new DateTime(2020,3,2)),
+    new Mark("Daniela","Drozdová", "P2", "MAT", 5, new DateTime(2020,2,3))
+};
+
+// Seřaďte záznamy podle jména studenta
+// Seřaďte záznamy podle data
+// Seřaďte záznamy podle třídy sestupně
+// Získejte jen známky studentky Alice
+// Získejte jen známky z P1, nezískávejte ale jiné informace kromě příjmení a známky
+// Získejte jen známky z P1, nezískávejte ale jiné informace kromě příjmení a známky
+// Získejte jen známky z P1 seřazené podle příjmení studenta
+// Získejte průměr všech známek studentky Alice
+// Získejte známky z jednotlivých předmětů studentky Alice
+// Získejte průměrné známky všech studentů
+// Získejte nejvyšší známky všech studentů
+// Seřaďte studenty podle jejich průměrných známek
