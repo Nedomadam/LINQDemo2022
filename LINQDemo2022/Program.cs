76b24511731d44697f83b46e3f4a8d92cbbd038f
Michal Stehlík <michal.stehlik@pslib.cz>
2022-01-09 21:53:40 +0100

    docs

--- LINQDemo2022/Program.cs
+++ LINQDemo2022/Program.cs
@@ -12,6 +12,18 @@ using LINQDemo2022.Model;
 // https://www.tutorialsteacher.com/linq
 // https://www.geeksforgeeks.org/linq-method-syntax/?ref=lbp
 
+// Where: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.where?view=net-6.0
+// Order: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.orderby?view=net-6.0
+// OrderByDescending: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.orderbydescending?view=net-6.0
+// Select: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.select?view=net-6.0
+// GroupBy: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.groupby?view=net-6.0
+// Count: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.count?view=net-6.0
+// Max: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.max?view=net-6.0
+// Min: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.min?view=net-6.0
+// Take: https://docs.microsoft.com/cs-cz/dotnet/api/system.linq.enumerable.take?view=net-6.0
+
+
+
 List<Mark> Marks = new List<Mark> { 
     new Mark("Alice","Andělová", "P1", "ALG", 1, new DateTime(2020,1,1)),
     new Mark("Bořivoj","Bohatý", "P1", "ALG", 3, new DateTime(2020,1,1)),
@@ -39,11 +51,13 @@ List<Mark> Marks = new List<Mark> {
 // Seřaďte záznamy podle data
 // Seřaďte záznamy podle třídy sestupně
 // Získejte jen známky studentky Alice
+// Získejte jen známky z P1
 // Získejte jen známky z P1, nezískávejte ale jiné informace kromě příjmení a známky
-// Získejte jen známky z P1, nezískávejte ale jiné informace kromě příjmení a známky
+// Kolik známek vlastně dostala dohromady třída P2?
 // Získejte jen známky z P1 seřazené podle příjmení studenta
 // Získejte průměr všech známek studentky Alice
 // Získejte známky z jednotlivých předmětů studentky Alice
 // Získejte průměrné známky všech studentů
+// Kolik známek dostala jaká třída?
 // Získejte nejvyšší známky všech studentů
 // Seřaďte studenty podle jejich průměrných známek
